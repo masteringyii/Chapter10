9237aaae0ae4eb2fe687e13e6345fd64d91fc415
Charles R. Portwood II <charlesportwoodii@ethreal.net>
2015-09-18 18:46:22 +0000

    Removing unecessary files for Chapter 9 template

--- views/site/login.php
+++
@@ -1,5 +0,0 @@
-<div class="site-login" style="margin-top: 100px";>
-    <div class="body-content">
-        <?php echo $this->render('forms/LoginForm', [ 'model' => $model ]); ?>
-    </div>
-</div>
--- views/site/register.php
+++
@@ -1,5 +0,0 @@
-<div class="site-login" style="margin-top: 100px";>
-    <div class="body-content">
-        <?php echo $this->render('forms/RegisterForm', [ 'model' => $model ]); ?>
-    </div>
-</div>
