9bc0cafd05ce0baf3775e3ce3e1ffdedfdbb6036
Charles R. Portwood II <charlesportwoodii@ethreal.net>
2015-10-04 19:21:10 +0000

    Removing relational data from migrations

--- migrations/m150523_203944_relational.php
+++ migrations/m150523_203944_relational.php
@@ -39,25 +39,6 @@ class m150523_203944_relational extends Migration
             'updated_at'   => Schema::TYPE_INTEGER,
 			'FOREIGN KEY(author_id) REFERENCES user(id)'
 		]);
-
-		// Insert some dummy data
-	
-		$this->batchInsert('role', ['id', 'name'], [
-			[1, 'User'], [2, 'Admin']
-		]);
-
-		$this->batchInsert('user', ['id', 'email', 'password', 'first_name', 'last_name', 'role_id', 'created_at', 'updated_at'], [
-			[1, 'jane.doe@example.com', password_hash('password1', PASSWORD_BCRYPT, ['cost' => 13]), 'Jane', 'Joe', 1, time(), time()],
-			[2, 'john.doe@example.com', password_hash('password2', PASSWORD_BCRYPT, ['cost' => 13]), 'John', 'Joe', 2, time(), time()],
-			[3, 'johnie.doe@example.com', password_hash('password3', PASSWORD_BCRYPT, ['cost' => 13]), 'Johnie', 'Joe', 1, time(), time()],
-			[4, 'admin@example.com', password_hash('admin', PASSWORD_BCRYPT, ['cost' => 13]), 'Site', 'Administrator', 2, time(), time()]
-		]);
-
-		$this->batchInsert('post', ['title', 'slug', 'content', 'author_id', 'created_at', 'updated_at'], [
-			['Lorem Ipsum', '/lorem-ipsum', 'Lorem Ipsum Example Post', 4, time(), time()],
-			['Lorem Ipsum 2', '/lorem-ipsum-2', 'Lorem Ipsum Example Post 2', 4, time(), time()],
-			['Lorem Ipsum 3', '/lorem-ipsum-3', 'Lorem Ipsum Example Post 3', 4, time(), time()],
-		]);
     }
     
     public function safeDown()
